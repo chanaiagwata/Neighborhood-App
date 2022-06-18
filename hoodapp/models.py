09868e806d8a1d296d650a7c6d6f65b3fb861333
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-18 23:47:17 +0300

    feat:adds models

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name=models.CharField(max_length=250)
+    location = models.CharField(max_length=250, null=False)
+    hood_pic = models.ImageField(upload_to = 'posts/')
+    description = models.TextField(max_length=255)
+    health_officer = models.CharField(blank=True, max_length=250)
+    health_contact =models.IntegerField()
+    police_officer  = models.CharField(blank=True, max_length=250)
+    police_contact  = models.IntegerField()
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    hoodname = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(max_length=240, null=True)
+    location = models.CharField(max_length=255, null=False)
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=255)
+    email = models.EmailField(max_length=250)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    hood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    description = models.TextField(max_length=255)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    
+
+
+
+
+
