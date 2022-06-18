c13089cbca66570f381a67f6207601766e2a7d78
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-18 19:33:56 +0300

    feat:adds navbar

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    '''
+    Function that renders the landing page
+    '''
+    return render(request, 'index.html')
